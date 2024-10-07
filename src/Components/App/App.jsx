5eed5c226330789a75057e26eabb55f0bc9f706d
Crisandry Javier <crisandryj@gmail.com>
2024-10-07 14:53:30 -0400

    Feat: Fetch data from api

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import Card from "../Card/Card";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    // fetch data
+    const dataFetch = async () => {
+      const data = await (
+        await fetch("https://fakestoreapi.com/products")
+      ).json();
+
+      // set state when the data received
+      setData(data);
+    };
+
+    dataFetch();
+  }, []);
+
+  console.log(data);
 
   return (
     <>
       <NavBar></NavBar>
-      <body>
+      <div>
         <Card></Card>
-      </body>
+      </div>
     </>
   );
 }
