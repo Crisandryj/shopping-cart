70fba37c33378a6b2119489e6f89d65fd7238fd9
Crisandry Javier <crisandryj@gmail.com>
2024-11-01 09:33:47 -0400

    Refactor: Group + - with quantity

--- src/Components/CartItem/CartItem.jsx
+++ src/Components/CartItem/CartItem.jsx
@@ -3,12 +3,12 @@ import styles from "./CartItem.module.css";
 function CartItem(props) {
   return (
     <div className={styles.item}>
-      {console.log(props.image)}
       <img className={styles.product} src={props.image} alt="" />
-      <button>+</button>
-      <button>-</button>
-      <p>QTY</p>
-      <p>Total</p>
+      <div>
+        <p>QTY</p>
+        <button>+</button>
+        <button>-</button>
+      </div>
     </div>
   );
 }
