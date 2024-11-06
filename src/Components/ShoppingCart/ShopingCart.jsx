39b77caf58058defdb1eb1878dfb11f0c2001317
Crisandry Javier <crisandryj@gmail.com>
2024-11-06 14:31:06 -0500

    Refactor: Remove ul from cartlist and add to render

--- src/Components/ShoppingCart/ShopingCart.jsx
+++ src/Components/ShoppingCart/ShopingCart.jsx
@@ -4,12 +4,10 @@ function ShoppingCart(props) {
   let total = 0;
 
   const cartList = props.cart.map((item) => (
-    <ul>
-      <li key={item.id}>
-        <CartItem image={item.item.image} />
-        {(total += item.item.price)}
-      </li>
-    </ul>
+    <li className="hello" key={item.id}>
+      <CartItem image={item.item.image} />
+      {(total += item.item.price)}
+    </li>
   ));
 
   if ({ cartList }.cartList.length == 0) {
@@ -18,7 +16,7 @@ function ShoppingCart(props) {
 
   return (
     <div>
-      {cartList}
+      <ul>{cartList}</ul>
       <p>Total: ${total}</p>
     </div>
   );
