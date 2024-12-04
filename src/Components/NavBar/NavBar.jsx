65e648178160ce0065d265e29f98466dc5bc7355
Crisandry Javier <crisandryj@gmail.com>
2024-12-04 11:59:01 -0500

    Feat: Add new onclick commands and new button for shopping

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,21 @@
 import styles from "./NavBar.module.css";
 import shopCart from "../../assets/shopping-cart.svg";
 import home from "../../assets/home.svg";
+import bag from "../../assets/shopping-bag.svg";
 
 function NavBar(props) {
   return (
     <div className={styles.container}>
-      <button onClick={props.onClick}>
+      <button onClick={props.onClickCart}>
         <img src={shopCart} className="logo-shop" alt="React logo" />
       </button>
-      <button onClick={props.onClick}>
+      <button onClick={props.onClickHome}>
         {" "}
         <img src={home} className="logo-shop" alt="React logo" />
+      </button>
+      <button onClick={props.onClickShopping}>
+        {" "}
+        <img src={bag} className="logo-shop" alt="React logo" />
       </button>
     </div>
   );
