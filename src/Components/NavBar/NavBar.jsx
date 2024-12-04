293773ea564a0b83fb84bee5b35b0dc5a50b0eca
Crisandry Javier <crisandryj@gmail.com>
2024-12-04 12:10:15 -0500

    Refactor: reorder nav bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,16 +6,16 @@ import bag from "../../assets/shopping-bag.svg";
 function NavBar(props) {
   return (
     <div className={styles.container}>
-      <button onClick={props.onClickCart}>
-        <img src={shopCart} className="logo-shop" alt="React logo" />
-      </button>
-
       <button onClick={props.onClickHome}>
         <img src={home} className="logo-shop" alt="React logo" />
       </button>
 
       <button onClick={props.onClickShopping}>
         <img src={bag} className="logo-shop" alt="React logo" />
+      </button>
+
+      <button onClick={props.onClickCart}>
+        <img src={shopCart} className="logo-shop" alt="React logo" />
       </button>
     </div>
   );
